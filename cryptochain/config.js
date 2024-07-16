6cda3303943b0d227f56833e6198964575ef711c
aldokuritsu <47854856+aldokuritsu@users.noreply.github.com>
2024-07-16 22:38:04 +0200

    Ajout tests genesis block

---
+++ cryptochain/config.js
@@ -0,0 +1,9 @@
+// Création du 1er bloc qui contient des données fixes
+const GENESIS_DATA = {
+    timestamp: 1,
+    lastHash: '-----',
+    hash: 'hash-one',
+    data: []
+} ;
+
+module.exports = {GENESIS_DATA};
